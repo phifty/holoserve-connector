79b03eaaceb58a6a51657fe1ddc37385b6379649
Philipp Brüll <philipp.bruell@skrill.com>
2011-12-09 15:00:36 +0100

    fixed tests to fit new server version

--- features/step_definitions/bucket_steps.rb
+++ features/step_definitions/bucket_steps.rb
@@ -1,4 +1,6 @@
 
 Then /^the bucket should contain the ((test|unhandled) (post|put|get|delete) request)$/ do |request, type, method|
-  client.bucket.requests.should include(request)
+  client.bucket.requests.inject true do |result, request_in_bucket|
+    result && Request::Matcher.new(request_in_bucket, request).match?
+  end.should be_true
 end
